39e1038173b69ed1b8b857b77874b982f09f3ee5
finxol <personnal@bottleneck.anonaddy.com>
2022-03-13 23:44:10 +0100

    make form usable

--- components/TripForm.vue
+++ components/TripForm.vue
@@ -2,6 +2,7 @@
     <v-form
         ref="form"
         v-model="valid"
+        @keyup.native.enter="valid && validate($event)"
         lazy-validation
     >
         <v-row>
@@ -9,7 +10,9 @@
                 v-model="from"
                 label="Lieu de départ"
                 placeholder="Nom de ville"
+                :rules="rules"
                 filled
+                required
             ></v-text-field>
         </v-row>
         <v-row>
@@ -17,7 +20,9 @@
                 v-model="to"
                 label="Lieu d'arrivée"
                 placeholder="Nom de ville"
+                :rules="rules"
                 filled
+                required
             ></v-text-field>
         </v-row>
         <v-row>
@@ -33,20 +38,22 @@
                 <template v-slot:activator="{ on, attrs }">
                     <v-text-field
                         v-model="dateFormatted"
-                        label="Date"
-                        hint="MM/DD/YYYY format"
+                        label="Date de départ"
+                        hint="Au format JJ/MM/AAAA"
                         persistent-hint
                         prepend-icon="mdi-calendar"
                         v-bind="attrs"
                         @blur="date = parseDate(dateFormatted)"
                         v-on="on"
                         filled
+                        :rules="rules"
                     ></v-text-field>
                 </template>
                 <v-date-picker
                     v-model="date"
                     no-title
                     @input="menu1 = false"
+                    required
                 ></v-date-picker>
             </v-menu>
         </v-row>
@@ -76,6 +83,9 @@ export default {
             dateFormatted: this.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substring(0, 10)),
             menu1: false,
             menu2: false,
+            rules: [
+                v => !!v || 'Merci de renseigner ce champ',
+            ],
         }
     },
     watch: {
@@ -91,12 +101,12 @@ export default {
             if (!date) return null;
 
             const [year, month, day] = date.split('-');
-            return `${month}/${day}/${year}`
+            return `${day}/${month}/${year}`
         },
         parseDate (date) {
             if (!date) return null;
 
-            const [month, day, year] = date.split('/');
+            const [day, month, year] = date.split('/');
             return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
         },
     }
