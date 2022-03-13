f465833ccefc00118d5d08618075897e3edd63cc
finxol <personnal@bottleneck.anonaddy.com>
2022-03-13 23:43:29 +0100

    add modules & render & pwa & other features

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,15 @@
 import colors from 'vuetify/es5/util/colors'
 
+const PLAUSIBLE_DOMAIN = ''
+const DESCRIPTION = 'Plateforme de covoiturage entre étudiants'
+const TITLE = 'UICovoit'
+const META_TITLE = `${TITLE} | IUCovoit`
+const DOMAIN = 'iucovoit.com'
+const URL = 'https://' + DOMAIN
+const BANNER = `${URL}/wave.svg`
+
 export default {
+    telemetry: false,
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
     ssr: true,
 
@@ -15,10 +24,16 @@ export default {
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
             {hid: 'description', name: 'description', content: 'Plateforme de covoiturage pour IUT'},
-            {name: 'format-detection', content: 'telephone=no'}
+            {name: 'format-detection', content: 'telephone=no'},
+            {
+                name: 'keywords',
+                content: 'covoiturage,blablacar,étudiant,dut informatique,dut,iutvannes,iut de vannes,iut vannes'
+            },
+            {name: 'author', content: 'finxol'},
+            {name: 'language', content: 'French'},
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/wave.svg'}
+            {rel: 'icon', type: 'image/svg', href: '/wave.svg'}
         ]
     },
 
@@ -31,22 +46,82 @@ export default {
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
 
+    // Server Middleware
+    serverMiddleware: [
+        {path: '/api/v1', handler: '~/server/'}
+    ],
+
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/typescript
         '@nuxt/typescript-build',
         // https://go.nuxtjs.dev/vuetify
         '@nuxtjs/vuetify',
+        // https://go.nuxtjs.dev/pwa
+        '@nuxtjs/pwa',
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [],
+    modules: [
+        // https://www.npmjs.com/package/cookie-universal-nuxt
+        'cookie-universal-nuxt',
+        [
+            '@dansmaculotte/nuxt-security',
+            {
+                hsts: {
+                    maxAge: 15552000,
+                    includeSubDomains: true,
+                    preload: true
+                },
+                referrer: 'same-origin',
+                additionalHeaders: true
+            }
+        ]
+    ],
+
+    render: {
+        csp: {
+            hashAlgorithm: 'sha256',
+            policies: {
+                'default-src': ["'self'"],
+                'style-src': ["'self'", "'unsafe-inline'", 'fonts.googleapis.com', 'cdn.jsdelivr.net'],
+                'font-src': ['fonts.googleapis.com', 'fonts.gstatic.com'],
+                'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+                'connect-src': ["'self'", PLAUSIBLE_DOMAIN]
+            }
+        }
+    },
+
+    pwa: {
+        meta: {
+            title: TITLE,
+            author: 'kernoeb',
+            description: DESCRIPTION,
+            lang: 'fr',
+            ogSiteName: TITLE,
+            ogTitle: TITLE,
+            ogDescription: DESCRIPTION
+        },
+        manifest: {
+            name: TITLE,
+            short_name: TITLE,
+            description: DESCRIPTION,
+            lang: 'fr',
+            display: 'standalone'
+        },
+        workbox: {
+            cleanupOutdatedCaches: true
+        }
+    },
 
     // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
     vuetify: {
         customVariables: ['~/assets/variables.scss'],
         theme: {
             dark: false,
+            lang: {
+                current: 'fr'
+            },
             themes: {
                 dark: {
                     primary: "#D1E2F4",
