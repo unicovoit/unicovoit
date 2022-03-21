91ef7acc9568d3f637b0ca59e76e740d0bf37e3e
finxol <personnal@bottleneck.anonaddy.com>
2022-03-21 22:51:46 +0100

    dynamically build cards

--- pages/trips.vue
+++ pages/trips.vue
@@ -1,5 +1,22 @@
 <template>
-    <v-container>
+    <v-container
+        v-if="isEmpty()"
+    >
+        <v-card
+            v-for='(trip, index) in trips'
+            :key='index'
+        >
+            <v-card-title>
+                {{ trip.from }}
+                <v-icon color="text--primary">mdi-arrow-right-bold-circle-outline</v-icon>
+                {{ trip.to }}
+            </v-card-title>
+            <v-card-subtitle>
+                {{ trip.price }} €
+            </v-card-subtitle>
+        </v-card>
+    </v-container>
+    <v-container v-else>
         <v-card>
             <v-card-title>
                 Aucun trajet disponible
@@ -10,6 +27,18 @@
 
 <script>
 export default {
-    name: 'Trajets'
+    name: 'Trajets',
+    data () {
+        return {
+            trips:[
+                {from: "Rennes", to: "Vannes", price: 0}
+            ],
+        }
+    },
+    methods: {
+        isEmpty() {
+            return this.trips.length > 0
+        }
+    },
 }
 </script>
