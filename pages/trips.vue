e22be6be4e13b97c3683ff95a5f42e0f5572079c
finxol <personnal@bottleneck.anonaddy.com>
2022-03-28 16:11:32 +0200

    first trips page build

--- pages/trips.vue
+++ pages/trips.vue
@@ -1,25 +1,60 @@
 <template>
     <v-container
-        v-if="isEmpty()"
+        v-if="!isEmpty"
     >
         <v-card
             v-for='(trip, index) in trips'
             :key='index'
-            elevation="4"
+            outlined
         >
             <v-card-title>
-                {{ trip.from }}
+                {{ trip.from }}&nbsp;
                 <v-icon color="text--primary">mdi-arrow-right-bold</v-icon>
-                {{ trip.to }}
+                &nbsp;{{ trip.to }}
             </v-card-title>
             <v-card-subtitle>
                 {{ trip.price }} €
             </v-card-subtitle>
+            <v-card-actions
+                class="accent lighten-2"
+            >
+                <v-avatar
+                    size="40"
+                >
+                    <v-img
+                        :src="trip.driver.picture || '/account_circle.svg'"
+                        :alt="trip.driver.name"
+                        @click="$router.push(`/profile/${trip.driver.id}`)"
+                    ></v-img>
+                </v-avatar>
+                &nbsp;
+                {{ trip.driver.name || '' }}
+                <v-spacer></v-spacer>
+                <v-btn
+                    icon
+                    @click="trip.show = !trip.show"
+                >
+                    <v-icon>mdi-chevron-{{ trip.show ? 'up' : 'down' }}</v-icon>
+                </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+                <div
+                    v-show="trip.show"
+                    class="accent lighten-2"
+                >
+                    <v-card-text>
+                        {{ trip.description }}
+                    </v-card-text>
+                </div>
+            </v-expand-transition>
         </v-card>
     </v-container>
+
+    <!-- Show no trips are available -->
     <v-container v-else>
         <v-card
-            elevation="4"
+            outlined
         >
             <v-card-title>
                 Aucun trajet disponible
@@ -37,21 +72,29 @@ export default {
         }
     },
     created() {
-        /*this.getTrips();*/
+        this.getTrips();
+    },
+    computed: {
+        isEmpty() {
+            return this.trips.length === 0;
+        },
     },
     methods: {
-        /*getTrips() {
-            axios.get('/api/trips')
+        getTrips() {
+            this.$axios.get('/api/v1/trips')
                 .then(response => {
                     this.trips = response.data;
                 })
                 .catch(error => {
                     console.log(error);
                 });
-        },*/
-        isEmpty() {
-            return this.trips.length > 0;
-        }
+        },
     }
 }
 </script>
+
+<style>
+.v-card {
+    margin: 0 auto 1.5rem;
+}
+</style>
