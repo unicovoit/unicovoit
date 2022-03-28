3fb82a94b640d919b29667a42b4586debbe9d7d1
finxol <personnal@bottleneck.anonaddy.com>
2022-03-28 16:11:10 +0200

    hard code trips for tests

--- api/routes/trips.ts
+++ api/routes/trips.ts
@@ -1,6 +1,7 @@
-import { Router } from 'express'
-const router: Router = Router()
+import {Router} from 'express'
 import logger from '../util/signale'
+
+const router: Router = Router()
 
 const Trip = require('../models/Trip')
 
@@ -9,19 +10,54 @@ const Trip = require('../models/Trip')
 // @access  Public
 // @ts-ignore
 router.get('/', (req: Request<RouteParameters<string>, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>) => {
+    res.json([{
+        id: 1,
+        driver: {
+            id: 0,
+            name: "du7iuw5f",
+            picture: "https://s.gravatar.com/avatar/371bf211f9b892f400479cb44bb6f1cf?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fdu.png"
+        },
+        from: 'Paris',
+        to: 'Marseille',
+        price: '100',
+        description: 'Accompagnez-moi sur la route de Marseille. J\'ai un petit coffre donc petit bagages svp.',
+        show: false,
+    },
+        {
+            id: 2,
+            driver: {
+                id: 1,
+                name: "finxol",
+                picture: "https://cdn.discordapp.com/avatars/688822573970096165/b9480f354ea3fbaf05abb964265a1cc8.png"
+            },
+            from: 'Paris',
+            to: 'Lyon',
+            price: '200',
+            description: 'Accompagnez-moi sur la route de Lyon. J\'ai un petit coffre donc petit bagages svp.',
+            show: false,
+        },
+        {
+            id: 3,
+            driver: {id: 2, name: "Elouann", picture: ""},
+            from: 'Paris',
+            to: 'Bordeaux',
+            price: '300',
+            description: 'Accompagnez-moi sur la route de Bordeaux. J\'aime beaucoup discuter et écouter de la musique. Voyage non fumeur.',
+            show: false,
+        }])
     try {
         Trip.find()
             .sort({date: -1})
             .then((trip: object) => res.json(trip))
             .catch((err: Error) => {
                 // @ts-ignore
-                res.status(404).json({ nopostsfound: 'No trips found' })
+                res.status(404).json({nopostsfound: 'No trips found'})
                 logger.error(err)
             })
         console.log(Trip)
     } catch (e) {
         logger.error(e)
-        res.status(500).json({ error: 'Internal server error' })
+        res.status(500).json({error: 'Internal server error'})
     }
 })
 
