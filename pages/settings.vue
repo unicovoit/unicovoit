5f51aeee59c1a88901ddc05c31aae77b29eb3090
finxol <personnal@bottleneck.anonaddy.com>
2022-03-29 22:18:37 +0200

    create settings page

---
+++ pages/settings.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container>
+        <v-main
+            class="text-h2"
+            color="primary"
+            style="margin-top:-3rem"
+        >
+            Paramètres
+        </v-main>
+
+        <v-card
+            outlined
+            @click="toggleTheme"
+        >
+            <v-container>
+                <v-icon>mdi-theme-light-dark</v-icon>&nbsp;
+                Passer en mode {{ $vuetify.theme.dark ? 'clair' : 'sombre' }}
+            </v-container>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "settings",
+    auth: false,
+    methods: {
+        toggleTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+            try {
+                this.$cookies.set('dark', `${this.$vuetify.theme.dark}`, {
+                    path: '/',
+                    sameSite: 'Strict'
+                })
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
