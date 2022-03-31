054cb9521a644efdaa85fc433accb28c4deb80b2
finxol <personnal@bottleneck.anonaddy.com>
2022-04-01 01:36:55 +0200

    add book trip method

--- api/util/db.ts
+++ api/util/db.ts
@@ -17,3 +17,22 @@ export const addTrip: Function = async (t: object) => {
     logger.info('------------------')
     logger.info(countAdd + ' added elements')
 }
+
+
+export const bookTrip: Function = async (t: object) => {
+    const {Trip} = require('../models/Trip')
+
+    let countBook = 0
+
+    try {
+        const tmp = new Trip(t)
+        await tmp.save()
+        logger.log('Booked : ' + tmp.fullId)
+        countBook++
+    } catch (err) {
+        throw err
+    }
+
+    logger.info('------------------')
+    logger.info(countBook + ' booked elements')
+}
