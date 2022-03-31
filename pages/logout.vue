18db2f26adef0096fde63b1b8ab642aeb356129d
finxol <personnal@bottleneck.anonaddy.com>
2022-03-31 02:57:27 +0200

    add logout page

---
+++ pages/logout.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container>
+    <v-main
+        class="text-h2 mt-n10"
+        color="primary"
+    >
+        Déconnexion...
+    </v-main>
+</v-container>
+</template>
+
+<script>
+export default {
+    name: "logout",
+    mounted() {
+        this.$auth.logout()
+        this.$router.go(-1)
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
