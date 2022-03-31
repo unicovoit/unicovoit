0faa201ea0a426672fe28c8956be6af6b62c7def
finxol <personnal@bottleneck.anonaddy.com>
2022-03-31 02:52:31 +0200

    add "add trip" page

---
+++ pages/trips/add.vue
@@ -0,0 +1,46 @@
+<template>
+<v-container>
+    <v-main
+        class="text-h2 mt-n10"
+        color="primary"
+    >
+        Proposer un trajet
+    </v-main>
+    <v-form>
+        <v-row>
+            <v-text-field
+                filled
+                label="Départ"
+                v-model="trip.from"
+                prepend-inner-icon="mdi-map-marker"
+            ></v-text-field>
+        </v-row>
+        <v-row>
+            <v-text-field
+                filled
+                label="Arrivée"
+                v-model="trip.to"
+                append-icon="mdi-map-marker"
+            ></v-text-field>
+        </v-row>
+    </v-form>
+</v-container>
+</template>
+
+<script>
+export default {
+    name: "add",
+    data: () => ({
+        trip: {
+            from: "",
+            to: "",
+            price: "",
+            description: "",
+        }
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
