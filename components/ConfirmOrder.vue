cf5b6b70d0cfc453db9da7d764632110d5687d11
finxol <personnal@bottleneck.anonaddy.com>
2022-04-01 01:31:59 +0200

    add distance + confirm order button

---
+++ components/ConfirmOrder.vue
@@ -0,0 +1,162 @@
+<template>
+    <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                v-bind="attrs"
+                v-on="on"
+                block
+                class="mt-4"
+                color="info"
+                x-large
+            >
+                Réserver
+            </v-btn>
+        </template>
+        <v-card>
+            <v-toolbar
+                dark
+                color="info"
+            >
+                <v-btn
+                    icon
+                    dark
+                    @click="dialog = false"
+                >
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+                <v-toolbar-title>Réserver un trajet</v-toolbar-title>
+            </v-toolbar>
+
+            <v-alert
+                v-if="success"
+                class="mx-2 my-4"
+                border="left"
+                type="success"
+            >
+                Trajet réservé !
+            </v-alert>
+            <v-alert
+                v-if="error"
+                class="mx-2 my-4"
+                border="left"
+                type="error"
+            >
+                Une erreur est survenue. Merci de réessayer plus tard.
+            </v-alert>
+
+            <v-alert
+                class="mt-4 mx-2"
+                icon="mdi-information"
+                type="warning"
+                border="left"
+                text
+            >
+                UniCovoit ne gère pas les paiements en ligne.
+                C'est à vous de convenir d'un moyen de paiement avec le conducteur.
+            </v-alert>
+
+            <v-list
+                subheader
+            >
+                <v-subheader>Trajet</v-subheader>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ trip.from }}</v-list-item-title>
+                        <v-list-item-subtitle>Départ</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ trip.to }}</v-list-item-title>
+                        <v-list-item-subtitle>Arrivée</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ parseDate }}</v-list-item-title>
+                        <v-list-item-subtitle>Date</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ trip.price }} €</v-list-item-title>
+                        <v-list-item-subtitle>Prix du trajet</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ this.trip.distance }} km</v-list-item-title>
+                        <v-list-item-subtitle>Distance</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-divider></v-divider>
+
+            <v-container class="mt-5 px-8">
+                <v-btn
+                    color="info"
+                    justify="center"
+                    block
+                    x-large
+                    @click="confirm"
+                >
+                    Confirmer la réservation
+                </v-btn>
+            </v-container>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    name: "ConfirmOrder",
+    props: {
+        trip: {
+            type: Object,
+            required: true
+        },
+    },
+    data() {
+        return {
+            dialog: false,
+            distance: 0,
+            error: false,
+            success: false,
+        };
+    },
+    computed: {
+        parseDate() {
+            const date = new Date(this.trip.date);
+            let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+            return date.toLocaleDateString('fr-FR', options);
+        }
+    },
+    methods: {
+        confirm() {
+            this.$axios.post('/api/v1/trips/book', {
+                body: {
+                    trip_id: this.trip.id,
+                    user_id: this.$store.state.auth.user.sub
+                }
+            }).then(response => {
+                this.success = true
+                this.error = false
+            }).catch(error => {
+                this.error = true
+                this.success = false
+                console.error(error)
+            })
+        },
+    }
+}
+</script>
+
+<style scoped lang="sass">
+.v-list-item__title
+    text-transform: capitalize
+</style>
--- pages/trips/_id.vue
+++ pages/trips/_id.vue
@@ -1,112 +1,142 @@
 <template>
     <v-container>
-        <div
+        <v-main
+            class="text-h2 mt-n10 mb-n3"
+            color="primary"
         >
-            <v-main
-                class="text-h2 mt-n10"
-                color="primary"
-            >
-                Trajet
-            </v-main>
+            Trajet
+        </v-main>
 
-            <v-timeline
-                class="mb-10"
-                dense
-                justify="center"
+        <v-timeline
+            appear="slide-y-transition"
+            class="mb-8"
+            dense
+            justify="center"
+            large
+        >
+            <v-timeline-item
+                color="info"
+                fill-dot
+                icon="mdi-map-marker"
                 large
             >
-                <v-timeline-item
-                    color="info"
-                    icon="mdi-map-marker"
-                    fill-dot
-                    large
-                >
-                    <v-card flat>
-                        <v-card-title class="text-h5">
-                            {{ trip.from }}
-                        </v-card-title>
-                    </v-card>
-                </v-timeline-item>
-                <v-timeline-item
-                    color="info"
-                    icon="mdi-map-marker"
-                    fill-dot
-                    large
-                >
-                    <v-card flat>
-                        <v-card-title class="text-h5">
-                            {{ trip.to }}
-                        </v-card-title>
-                    </v-card>
-                </v-timeline-item>
-            </v-timeline>
-
-            <v-card
-                outlined
-                class="mt-3"
+                <v-card flat>
+                    <v-card-title class="text-h5">
+                        {{ trip.from }}
+                    </v-card-title>
+                </v-card>
+            </v-timeline-item>
+            <v-timeline-item
+                color="info"
+                fill-dot
+                icon="mdi-map-marker"
+                large
             >
-                <v-card-text
-                    class="text--primary"
-                >
-                    {{ trip.description }}
-                </v-card-text>
-                <v-card-text>
-                    <v-avatar
-                        size="40"
-                    >
-                        <v-img
-                            :alt="trip.driver.name || 'Utilisateur'"
-                            :src="trip.driver.picture || '/account_circle.svg'"
-                            @click="$router.push(`/profile/${trip.driver.id}`)"
-                        ></v-img>
-                    </v-avatar>
-                    &nbsp;{{ trip.driver.name || 'Utilisateur' }}
-                    <v-spacer></v-spacer>
-                </v-card-text>
-            </v-card>
+                <v-card flat>
+                    <v-card-title class="text-h5">
+                        {{ trip.to }}
+                    </v-card-title>
+                </v-card>
+            </v-timeline-item>
+        </v-timeline>
 
-            <v-card
-                outlined
-                class="py-3 mt-3"
+
+        <v-card
+            class="mt-3"
+            outlined
+        >
+            <v-card-text
+                class="text--primary"
             >
-                <v-list-item justify="center">
-                    <v-list-item-content>
-                        <v-list-item-title>
-                            <v-icon class="mr-3">mdi-calendar-clock</v-icon>
-                            {{ parseDate }}
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item justify="center">
-                    <v-list-item-content>
-                        <v-list-item-title>
-                            <v-icon class="mr-3">mdi-currency-eur</v-icon>
-                            {{ trip.price }}
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-card>
-        </div>
+                {{ trip.description }}
+            </v-card-text>
+            <v-card-text>
+                <v-avatar
+                    size="40"
+                >
+                    <v-img
+                        :alt="trip.driver.name || 'Utilisateur'"
+                        :src="trip.driver.picture || '/account_circle.svg'"
+                        @click="$router.push(`/profile/${trip.driver.id}`)"
+                    ></v-img>
+                </v-avatar>
+                &nbsp;{{ trip.driver.name || 'Utilisateur' }}
+                <v-spacer></v-spacer>
+            </v-card-text>
+        </v-card>
+
+        <v-card
+            class="py-3 mt-3"
+            outlined
+        >
+            <v-list-item justify="center">
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <v-icon class="mr-3">mdi-calendar-clock</v-icon>
+                        {{ parseDate }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item justify="center">
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <v-icon class="mr-3">mdi-currency-eur</v-icon>
+                        {{ trip.price }} €
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <v-icon class="mr-3">mdi-map-marker-distance</v-icon>
+                        {{ trip.distance }} km
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-card>
+
+        <ConfirmOrder :trip="trip"></ConfirmOrder>
     </v-container>
 </template>
 
 <script>
+import ConfirmOrder from '../../components/ConfirmOrder'
+
 export default {
-    name: "book",
+    name: "trip",
+    components: {
+        ConfirmOrder,
+    },
     computed: {
         parseDate() {
             const date = new Date(this.trip.date);
             let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
             return date.toLocaleDateString('fr-FR', options);
-        }
+        },
     },
-    async asyncData({ error, params, $axios }) {
+    async asyncData({error, params, $axios}) {
         try {
             const trip = await $axios.$get(`/api/v1/trips/${params.id}`)
-            return { trip }
+            trip.distance = 0
+            return {trip}
         } catch (e) {
-            error({ statusCode: 404, message: 'Trip not found' })
+            console.error(e)
+            error({statusCode: 404, message: 'Trip not found'})
         }
+    },
+    async mounted() {
+        this.trip.distance = await this.$axios.$get('/api/v1/trips/distance', {
+                params: {
+                    from: this.trip.from,
+                    to: this.trip.to
+                }
+            }).then(response => {
+                console.log(response)
+                return response.distance
+            })
+            .catch(error => {
+                console.error(error)
+            })
     }
 }
 </script>
