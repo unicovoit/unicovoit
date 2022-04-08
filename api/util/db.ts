854b8774400b20a920c35eb75575ab849e14f683
finxol <personnal@bottleneck.anonaddy.com>
2022-04-08 11:58:19 +0200

    add user management

--- api/util/db.ts
+++ api/util/db.ts
@@ -1,7 +1,6 @@
 import logger from './signale'
 import {Trip} from '../models/Trip'
-// @ts-ignore
-import mongoose, {MongooseDocument} from 'mongoose'
+import {User} from '../models/User'
 
 
 export const testData = [{
@@ -41,11 +40,18 @@ export const testData = [{
     places: '5'
 }]
 
-
-
-export const addTrip: Function = async (t: object) => {
+//------------------------------------------------------
+// Trip-related functions
+//------------------------------------------------------
+/**
+ * Add a trip to the database
+ * @param t the trip to add
+ */
+export const addTrip: Function = async (t: any) => {
     logger.info(t)
     try {
+        t.from = t.from.toUpperCase()
+        t.to = t.to.toUpperCase()
         const tmp = new Trip(t)
         logger.info(tmp)
         await tmp.save()
@@ -55,6 +61,10 @@ export const addTrip: Function = async (t: object) => {
 }
 
 
+/**
+ * Book a trip
+ * @param t the trip to book
+ */
 export const bookTrip: Function = async (t: object) => {
     let countBook = 0
 
@@ -72,17 +82,40 @@ export const bookTrip: Function = async (t: object) => {
 }
 
 
+/**
+ * Get all trips
+ * @returns {Promise<Trip[]>} the array of trips
+ */
 export const getAllTrips = async () => {
     return await Trip.find({})
 }
 
 
+/**
+ * Get a trip by location and date
+ * @param from the departure location
+ * @param to the arrival location
+ * @param date the date of the trip
+ * @returns the array of trips
+ */
+export const getTrips = async (from: string, to: string, date: Date) => {
+    return await Trip.find({from: from.toUpperCase(), to: to.toUpperCase(), departure_time: {$gte: date}})
+}
+
+
+/**
+ * Get a trip by id
+ * @param id the id of the trip
+ * @return the trip
+ */
 export const getTripById = async (id: string) => {
     return Trip.findById(id)
 }
 
 
-
+/**
+ * Initialise the database
+ */
 export const initDB = async () => {
     try {
         // delete all saved trips
@@ -96,4 +129,58 @@ export const initDB = async () => {
     } catch (err) {
         logger.error(err)
     }
+}
+
+
+//------------------------------------------------------
+// User-related functions
+//------------------------------------------------------
+/**
+ * Add a user to the database
+ * @param u the user to add
+ */
+export const addUser: Function = async (u: object) => {
+    try {
+        const tmp = new User(u)
+        logger.info(tmp)
+        await tmp.save()
+    } catch (err) {
+        throw err
+    }
+}
+
+
+/**
+ * Get a user by id
+ * @param   id the id of the user
+ * @returns the user
+ */
+export const getUserById = async (id: string) => {
+    return User.findById(id)
+}
+
+
+/**
+ * Get user's picture url and name by id
+ * @param   id the id of the user
+ * @returns the picture url and name
+ */
+export const getUserAvatarAndNameById: Function = async (id: string) => {
+    return User.findById(id).then(user => {
+        return user ? {
+            picture: user.picture,
+            name: user.name
+        } : false
+    })
+}
+
+
+/**
+ * Update a user's picture
+ * @param   id the oauth id of the user
+ * @param   picture the new picture url
+ * @returns the updated user
+ */
+export const updateUserPicture = async (id: string, picture: string) => {
+    return User.findByIdAndUpdate({sub: id}, {picture: picture}, {new: true})
 }
