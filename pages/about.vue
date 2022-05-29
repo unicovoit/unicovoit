ec6c94b8fa1f6d128541851d8c109955f715b75a
finxol <personnal@bottleneck.anonaddy.com>
2022-05-30 01:24:08 +0200

    refactor about page

--- pages/about.vue
+++ pages/about.vue
@@ -7,35 +7,33 @@
             A Propos
         </v-main>
         <v-main>
-            UniCovoit est une initiative étudiante pour encourager le covoiturage à l'IUT.
-            <br>
-            <br>
-            <br>
-            Logoden biniou degemer mat, an. Beajourien dall kemmañ kaout, pegeit. Ober gwerzhañ Plouha bleun, kroaz.
-            Egineg diaes petra Brasparz, ni. Daouarn daol hervez stêr, ivez. Kuit buhez douar evel, envel. Gwec’h bihañ
-            piz kartoñs, ur. Digant torgenn Mederieg naer, c’hezeg. Egile emaout muiañ bemdez, eus. Karrezek menoz rak
-            kilpenn, degas.
-            <br>
-            <br>
-            C’hann pep c’higer meurzh, c’havr. Follenn hennezh koar livañ, bier. A izel Egineg tach, terriñ. Vaneg outañ
-            gallek Pleiber-Krist, davañjer. Keuz c’hreisteiz Melwenn onest, ampart. Soubañ fourchetez diaoul pounner,
-            kerzhout. Buoc’h Pempoull garm muioc’h, linenn. Abred dreist dale gwechall, kuz’h. Laerezh berr dant glas,
-            ken. Niz wec’h da feiz, brumenn.
-            <br>
-            <br>
-            Soubenn porpant ac'hanomp c’hreisteiz, klevout. Peoc’h bro Montroulez lizherenn, evezh. C’hilpenn ael c’hein
-            hi, Brieg. Señin an mevel soavon, Plougasnou. Ennañ digwener da ac'hanout, war. Skrigneg va Krouer gaou,
-            ret. Serret tavarn noazh daeroù, ken. Beleg koulz dre enez, bale. Huchal here outañ yec’hed, dirak .
-            Prenestr hep lagad anezhañ, diwezhat.
-            <br>
-            <br>
-            <br>
-            Made by <a
-                class="text--secondary"
-                href="https://github.com/finxol/unicovoit"
-                rel="noopener noreferrer"
-                target="_blank"
-            >finxol</a>
+            <p>
+                UniCovoit est une initiative étudiante pour encourager le covoiturage entre étudiants.
+            </p>
+            <p>
+                Ce service se base sur la confiance entre étudiants.
+                Les paiements ne se font pas par UniCovoit, mais directement entre passager et conducteur.
+            </p>
+            <p>
+                C’hann pep c’higer meurzh, c’havr. Follenn hennezh koar livañ, bier. A izel Egineg tach, terriñ. Vaneg outañ
+                gallek Pleiber-Krist, davañjer. Keuz c’hreisteiz Melwenn onest, ampart. Soubañ fourchetez diaoul pounner,
+                kerzhout. Buoc’h Pempoull garm muioc’h, linenn. Abred dreist dale gwechall, kuz’h. Laerezh berr dant glas,
+                ken. Niz wec’h da feiz, brumenn.
+            </p>
+            <p>
+                Soubenn porpant ac'hanomp c’hreisteiz, klevout. Peoc’h bro Montroulez lizherenn, evezh. C’hilpenn ael c’hein
+                hi, Brieg. Señin an mevel soavon, Plougasnou. Ennañ digwener da ac'hanout, war. Skrigneg va Krouer gaou,
+                ret. Serret tavarn noazh daeroù, ken. Beleg koulz dre enez, bale. Huchal here outañ yec’hed, dirak .
+                Prenestr hep lagad anezhañ, diwezhat.
+            </p>
+            <p>
+                Créer par <a
+                    class="text--secondary"
+                    href="https://github.com/finxol/unicovoit"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >finxol</a>
+            </p>
         </v-main>
     </v-container>
 </template>
