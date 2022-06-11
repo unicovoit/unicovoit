14e56484078659a52c6292d4b750224f3b348a64
finxol <personnal@bottleneck.anonaddy.com>
2022-06-11 03:33:34 +0200

    add privacy policy

---
+++ content/privacy-policy.md
@@ -0,0 +1,4 @@
+## Introduction
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eu blandit consectetur, nisi nisl aliquet
+nunc, euismod aliquam nunc nisl eu nisi.
---
+++ pages/privacy-policy.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <v-main
+            class="text-h2 mt-n10"
+            color="primary"
+        >
+            Politique de confidentialité
+        </v-main>
+
+        <nuxt-content
+            :document="page"
+            class="text-body-1 mt-5"
+        />
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "legal",
+    auth: false,
+    async asyncData({$content, error}) {
+        const page = await $content("privacy-policy")
+            .fetch()
+            .catch(err => {
+                error({statusCode: 404, message: "Page not found"})
+            })
+
+        return {
+            page: page
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+.nuxt-content
+    h2
+        text-align: left !important
+        margin-bottom: .5rem
+    p
+        text-align: justify !important
+</style>
