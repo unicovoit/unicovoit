b2bb7c5a7b1be764fa4e4fb4aa2fbe4b432e480c
finxol <personnal@bottleneck.anonaddy.com>
2022-07-04 22:13:21 +0200

    add profile fields

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,6 +1,12 @@
 import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema({
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true
+        },
         sub: {
             type: String,
             required: true,
@@ -18,15 +24,35 @@ const UserSchema = new mongoose.Schema({
         picture: {
             type: String
         },
+        smokePref: {
+            type: String,
+            default: "No"
+        },
+        petsPref: {
+            type: String,
+            default: "No"
+        },
+        musicPref: {
+            type: String,
+            default: "Yes"
+        },
+        autoBook: {
+            type: Boolean,
+            default: false
+        },
+        defaultPlaces: {
+            type: Number
+        },
         isVerified: {
             type: Boolean,
             default: false
         },
-        isBlocked: {
-            type: Boolean,
-            default: false
+        studentEmail: {
+            type: String,
+            required: true,
+            unique: true
         },
-        isBanned: {
+        isBlocked: {
             type: Boolean,
             default: false
         },
