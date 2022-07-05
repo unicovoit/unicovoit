2e2cfef07d411fe86408886579d169b0874bda08
finxol <personnal@bottleneck.anonaddy.com>
2022-07-05 03:42:06 +0200

    add multiple endpoints

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -14,37 +14,14 @@ const checkJwt = auth({
 
 
 /**
- * @route   GET /api/v1/users/:id/profile
- * @desc    Get user public profile by id
- * @access  Public
- */
-router.get('/:id/profile', async (req, res) => {
-    try {
-        const user = await db.getUserAvatarAndNameById(req.params.id)
-        if (user) {
-            res.status(200).json(user)
-        } else {
-            res.status(404).json({
-                error: 'User not found'
-            })
-        }
-    } catch (e) {
-        logger.error(e)
-        res.status(500).json({
-            error: 'Server error'
-        })
-    }
-})
-
-
-/**
- * @route   GET /api/v1/users/:id
+ * @route   GET /api/v1/users/
  * @desc    Get user by id
  * @access  Private
  */
-router.get('/:id', checkJwt, async (req, res) => {
+router.get('/', checkJwt, async (req, res) => {
     try {
-        const user = await db.getUserById(req.params.id)
+        // @ts-ignore
+        const user = await db.getUserById(req.auth.sub)
         if (user) {
             res.status(200).json(user)
         } else {
@@ -62,7 +39,106 @@ router.get('/:id', checkJwt, async (req, res) => {
 
 
 /**
- * @route   GET /api/v1/users/login
+ * @route   GET /api/v1/users/bookings
+ * @desc    Get user bookings
+ * @access  Private
+ */
+router.get('/bookings', checkJwt, async (req, res) => {
+    try {
+        // @ts-ignore
+        const trips = await db.getUserBookings(req.auth?.payload.sub)
+        if (trips) {
+            res.json(trips)
+        } else {
+            res.status(404).json({
+                error: 'User or bookings not found'
+            })
+        }
+    } catch (e: any) {
+        logger.error(e)
+        res.status(500).json({
+            error: isDev ? e.message : 'Server error'
+        })
+    }
+})
+
+
+/**
+ * @route   DELETE /api/v1/users/bookings/:id
+ * @desc    Delete user booking
+ * @access  Private
+ */
+router.delete('/bookings/:id', checkJwt, async (req, res) => {
+    try {
+        const trip = await db.deleteBooking(req.params.id, req.auth?.payload.sub)
+        if (trip) {
+            res.json(trip)
+        } else {
+            res.status(404).json({
+                error: 'User or booking not found'
+            })
+        }
+    } catch (e: any) {
+        logger.error(e)
+        res.status(500).json({
+            error: isDev ? e.message : 'Server error'
+        })
+    }
+})
+
+
+/**
+ * @route   GET /api/v1/users/trips
+ * @desc    Get user trips
+ * @access  Private
+ */
+router.get('/trips', checkJwt, async (req, res) => {
+    try {
+        const trips = await db.getUserTrips(req.auth?.payload.sub)
+        if (trips) {
+            res.json(trips)
+        } else {
+            res.status(404).json({
+                error: 'User or trips not found'
+            })
+        }
+    } catch (e: any) {
+        logger.error(e)
+        res.status(500).json({
+            error: isDev ? e.message : 'Server error'
+        })
+    }
+})
+
+
+/**
+ * @route   GET /api/v1/users/profile/:id
+ * @desc    Get user public profile by id
+ * @access  Public
+ */
+router.get('/profile/:id', async (req, res) => {
+    try {
+        const user = await db.getPublicProfile(req.params.id)
+        if (user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({
+                error: 'User not found'
+            })
+        }
+    } catch (e) {
+        logger.error(e)
+        res.status(500).json({
+            error: 'Server error'
+        })
+    }
+})
+
+
+
+
+/**
+ * @route   POST /api/v1/users/login
  * @desc    Login user and save to db
  * @access  Private
  */
@@ -112,17 +188,14 @@ router.post('/add', checkJwt, async (req, res) => {
 
 
 /**
- * @route   POST /api/v1/users/:id/picture
+ * @route   PUT /api/v1/users/picture
  * @desc    Update user picture
  * @access  Private
  */
-router.post('/:id/picture', checkJwt, async (req, res) => {
+router.put('/picture', checkJwt, async (req, res) => {
     try {
-        // @ts-ignore
         logger.info(req.body)
-        return res.status(200).json({
-            message: 'Picture updated'
-        })
+        return res.sendStatus(200)
         const user = await db.updateUserPicture(req.params.id, req.body)
         if (user) {
             res.status(200).json(user)
