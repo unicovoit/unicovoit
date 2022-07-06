8cf20fb325dba020e9d851246512169bf5c5b2eb
finxol <personnal@bottleneck.anonaddy.com>
2022-07-06 03:26:13 +0200

    add TripCard controls

---
+++ components/controls/Delete.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-container
+        class="ma-0 pa-0"
+    >
+        <v-btn
+            color="error"
+            icon
+            @click.prevent="confirmDeletion = true"
+        >
+            <v-icon size="30">mdi-delete</v-icon>
+        </v-btn>
+
+        <!-- Confirm booking suppression -->
+        <v-dialog
+            v-model="confirmDeletion"
+            max-width="500px"
+        >
+            <v-card>
+                <v-card-title>
+                    Voulez-vous vraiment supprimer cette réservation ?
+                </v-card-title>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        class="ma-1"
+                        color="primary"
+                        text
+                        @click.prevent="confirmDeletion = false"
+                    >
+                        Annuler
+                    </v-btn>
+
+                    <v-btn
+                        class="ma-1"
+                        color="error"
+                        @click="cancelBooking(trip.id)"
+                    >
+                        Supprimer
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "EditOrDelete",
+    props: {
+        trip: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            confirmDeletion: false,
+        };
+    },
+    methods: {
+        cancelBooking(id) {
+            this.$axios.delete(`/api/v1/users/bookings/${id}`)
+                .then(() => {
+                    this.$emit('refresh')
+                    this.confirmDeletion = false
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/controls/EditOrDelete.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-container
+        class="ma-0 pa-0"
+    >
+        <v-btn
+            color="primary"
+            icon
+            @click.prevent=""
+        >
+            <v-icon size="30">mdi-circle-edit-outline</v-icon>
+        </v-btn>
+        <v-btn
+            color="error"
+            icon
+            @click.prevent="confirmDeletion = true"
+        >
+            <v-icon size="30">mdi-delete</v-icon>
+        </v-btn>
+
+        <!-- Confirm booking suppression -->
+        <v-dialog
+            v-model="confirmDeletion"
+            max-width="500px"
+        >
+            <v-card>
+                <v-card-title>
+                    Voulez-vous vraiment supprimer ce trajet ?
+                </v-card-title>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        class="ma-1"
+                        color="primary"
+                        text
+                        @click.prevent="confirmDeletion = false"
+                    >
+                        Annuler
+                    </v-btn>
+
+                    <v-btn
+                        class="ma-1"
+                        color="error"
+                        @click="deleteTrip(trip.id)"
+                    >
+                        Supprimer
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "EditOrDelete",
+    props: {
+        trip: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            confirmDeletion: false,
+        };
+    },
+    methods: {
+        deleteTrip(id) {
+            this.$axios.delete(`/api/v1/trips/id/${id}`)
+                .then(() => {
+                    this.$emit('refresh')
+                    this.confirmDeletion = false
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
