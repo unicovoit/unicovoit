d265b42d43b26730426e6fd809f51a715fe88659
finxol <personnal@bottleneck.anonaddy.com>
2022-07-06 03:26:13 +0200

    add TripCard controls

---
+++ components/controls/ViewTrip.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-btn
+        color="primary"
+        icon
+    >
+        <v-icon size="35">mdi-car-arrow-right</v-icon>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "ViewTrip"
+}
+</script>
+
+<style scoped>
+
+</style>
