b4e0fb1b9a9cf282f85f595e4f8e356aaea9f109
finxol <personnal@bottleneck.anonaddy.com>
2022-07-07 23:15:11 +0200

    add version + uuid verification

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,22 @@
 // enable store for auth module
+const {version} = require('../package.json')
+import config from '../nuxt.config'
+
+const $config = config.publicRuntimeConfig
+
+export const state = () => ({
+    version: version,
+})
 
 export const getters = {
-    getCities: () => {
-    },
+    displayVersion: state => (state.version || '0') + ($config.isProd ? '' : '-dev')
+}
+
+export const mutations = {
+}
+
+export const actions = {
+    validateUuidV4(ctx, id: string) {
+        return /^[\dA-Fa-f]{8}-[\dA-Fa-f]{4}-4[\dA-Fa-f]{3}-[89ABab][\dA-Fa-f]{3}-[\dA-Fa-f]{12}$/i.test(id)
+    }
 }
