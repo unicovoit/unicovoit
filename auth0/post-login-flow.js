d208ab7eb4802b517afb1187e8c7b65184af7295
finxol <github@colinozanne.fr>
2022-07-11 14:18:10 +0200

    build post login action

--- auth0/post-login-flow.js
+++ auth0/post-login-flow.js
@@ -1,9 +1,37 @@
+const axios = require('axios')
+
 /**
 * @param {Event} event - Details about the user and the context in which they are logging in.
 * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
 */
 exports.onExecutePostLogin = async (event, api) => {
     // check if user is verified
+    const token = api.redirect.encodeToken({
+        secret: event.secrets.VERIFICATION_SECRET,
+        expiresInSeconds: 60, 
+        payload: {
+            sub: event.user.user_id,
+        },
+    });
+    axios.get('https://unicovoit.com/api/v1/users/isVerified', {
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    }).then(res => {
+        if (red.data.verified) {
+            // user is already verified, finish Action
+            return
+        } else {
+            // user isn't verified, redirect to verification page
+            api.redirect.sendUserTo("https://unicovoit.com/verify", {
+                query: { token: token }
+            });
+        }
+    }).catch(err => {
+        console.error(err)
+        api.access.deny("Une erreur est survenue. Merci de réessayer plus tard.")
+        return
+    })
 };
 
 
