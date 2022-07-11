4684eccf14a6a1ea0c3cee7619bee06a09e01231
finxol <github@colinozanne.fr>
2022-07-11 13:40:33 +0200

    Start post login auth0 action

---
+++ auth0/post-login-flow.js
@@ -0,0 +1,17 @@
+/**
+* @param {Event} event - Details about the user and the context in which they are logging in.
+* @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+*/
+exports.onExecutePostLogin = async (event, api) => {
+    // check if user is verified
+};
+
+
+
+
+/**
+* @param {Event} event - Details about the user and the context in which they are logging in.
+* @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
+*/
+exports.onContinuePostLogin = async (event, api) => {
+}
