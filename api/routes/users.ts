e31db75ec006154438bab5f3f0414cf583cf319b
finxol <personnal@bottleneck.anonaddy.com>
2022-07-27 19:49:51 +0200

    build logic for student status verification

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,8 +1,12 @@
 import * as db from '../util/db'
 import logger from '../util/signale'
+import {verifyEmail} from "../util/mail"
+import VerificationJWT from "../interfaces/VerificationJWT"
+
 import {auth} from "express-oauth2-jwt-bearer"
 import {Router} from "express"
 import * as jwt from 'jsonwebtoken'
+import * as crypto from "crypto"
 
 const router: Router = Router()
 
@@ -13,7 +17,7 @@ const checkJwt = auth({
     jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`,
     issuer: `https://${AUTH0_DOMAIN}/`,
     audience: `https://${AUTH0_DOMAIN}/api/v2/`,
-});
+})
 
 /**
  * Verify the JWT token comes from the Auth0 server
@@ -27,6 +31,7 @@ const originCheck = async (req, res, next) => {
         logger.info(token)
         const decoded = jwt.verify(token, VERIFICATION_SECRET)
         logger.info(decoded)
+        req.auth = {...decoded}
         next()
     } catch (e) {
         logger.error(e)
@@ -42,6 +47,17 @@ const originCheck = async (req, res, next) => {
  */
 router.get('/', checkJwt, async (req, res) => {
     try {
+        res.json({
+            "sub": "auth0|623f93c6c665610070aa3d75",
+            "nickname": "du7iuw5f",
+            "name": "du7iuw5f@mailer.me",
+            "picture": "https://s.gravatar.com/avatar/371bf211f9b892f400479cb44bb6f1cf?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fdu.png",
+            "updated_at": "2022-07-27T09:56:19.559Z",
+            "email": "du7iuw5f@mailer.me",
+            "email_verified": true,
+            "birthdate": new Date("2020-07-27T09:56:19.559Z").toLocaleDateString(),
+        })
+        return
         // @ts-ignore
         const user = await db.getUserById(req.auth.sub)
         if (user) {
@@ -189,11 +205,14 @@ router.post('/login', async (req, res) => {
  * @access  Private
  */
 router.get('/isVerified', originCheck, async (req, res, next) => {
-    logger.info(req)
     try {
-        // @ts-ignore
-        const verified: boolean = true || await db.userIsVerified(req.auth.sub)
-        res.status(200).json({verified})
+        const auth = req.auth as unknown as VerificationJWT
+        const verified: boolean | undefined = false // await db.userIsVerified(auth.sub)
+        const token = jwt.sign({
+            sub: auth.sub,
+        }, VERIFICATION_SECRET, { expiresIn: '5m' })
+
+        res.status(200).json({verified, token})
     } catch (e) {
         logger.error(e)
         res.status(500).json({
@@ -220,6 +239,71 @@ router.post('/add', checkJwt, async (req, res) => {
             })
         }
     } catch (e) {
+        logger.error(e)
+        res.status(500).json({
+            error: 'Server error'
+        })
+    }
+})
+
+
+/**
+ * @route   POST /api/v1/users/sendVerificationCode
+ * @desc    Send verification code to user
+ * @access  Private
+ */
+router.post('/sendVerificationCode', originCheck, async (req, res) => {
+    try {
+        const auth = req.auth as unknown as VerificationJWT
+        if (req.body.hasOwnProperty('email')) {
+            if (verifyEmail(String(req.body.email))) {
+                const bytes = crypto.randomBytes(4).toString('hex')
+                let code = String(parseInt(bytes, 16)).substring(0, 6)
+
+                await db.saveVerificationCode(auth.sub, req.body.email, code)
+                //TODO: send code by email
+
+                const token = jwt.sign({
+                    sub: auth.sub,
+                    email: req.body.email
+                }, VERIFICATION_SECRET, { expiresIn: '5m' })
+
+                logger.info(`Verification code: ${code}`)
+                res.json({token})
+            } else {
+                res.sendStatus(418)
+            }
+        } else {
+            res.sendStatus(400)
+        }
+    } catch (e) {
+        logger.error(e)
+        res.status(500).json({
+            error: 'Server error'
+        })
+    }
+})
+
+
+/**
+ * @route   POST /api/v1/users/verify
+ * @desc    Verify user
+ * @access  Private
+ */
+router.post('/verify', originCheck, async (req, res) => {
+    try {
+        logger.info(req.body)
+        logger.info(req.auth)
+        const auth = req.auth as unknown as VerificationJWT
+        if (req.body.hasOwnProperty('code') && /\d{6}/.test(req.body.code)) {
+
+                const ok = await db.verifyCode(auth.sub, String(auth.email), req.body.code)
+                res.sendStatus(ok ? 200 : 401)
+        } else {
+            res.sendStatus(400)
+        }
+    } catch
+        (e) {
         logger.error(e)
         res.status(500).json({
             error: 'Server error'
