607a2660378cfd26555a4ac1cb4a09b05a957e24
finxol <personnal@bottleneck.anonaddy.com>
2022-07-28 01:19:07 +0200

    complete post login flow

--- auth0/post-login-flow.js
+++ auth0/post-login-flow.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const url = 'https://unicovoit.fr'
 
 /**
 * @param {Event} event - Details about the user and the context in which they are logging in.
@@ -12,24 +13,42 @@ exports.onExecutePostLogin = async (event, api) => {
         payload: {
             sub: event.user.user_id,
         },
-    });
-    axios.get('https://unicovoit.com/api/v1/users/isVerified', {
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    }).then(res => {
-        if (res.data.verified) {
-            // user is already verified, finish Action
-        } else {
-            // user isn't verified, redirect to verification page
-            api.redirect.sendUserTo("https://unicovoit.com/verify", {
-                query: { token: token }
-            });
-        }
-    }).catch(err => {
-        console.error(err)
-        api.access.deny("Une erreur est survenue. Merci de réessayer plus tard.")
     })
+    console.log(token)
+    const user = {
+        ...event.user,
+        sub: event.user.user_id
+    }
+    console.log(user)
+    const profile = await axios.post(`${url}/api/users/profile/sub/${user.sub}`, {
+        user
+    }, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    if (profile.data.nickname && profile.data.picture) {
+        api.user.setUserMetadata('nickname', profile.data.nickname)
+        api.user.setUserMetadata('picture', profile.data.picture)
+    }
+
+    axios.get(`${url}/api/v1/users/isVerified`, {
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        }).then(res => {
+            if (res.data.verified) {
+                // user is already verified, finish Action
+            } else {
+                // user isn't verified, redirect to verification page
+                api.redirect.sendUserTo(`${url}/verify`, {
+                    query: { token }
+                })
+            }
+        }).catch(err => {
+            console.error(err)
+            api.access.deny("Une erreur est survenue. Merci de réessayer plus tard.")
+        })
 };
 
 
@@ -40,4 +59,13 @@ exports.onExecutePostLogin = async (event, api) => {
 * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
 */
 exports.onContinuePostLogin = async (event, api) => {
+    const data = api.redirect.validateToken({
+        secret: event.secrets.VERIFICATION_SECRET,
+        tokenParameterName: 'token',
+    })
+    if (data.verified && data.sub === event.user.user_id) {
+        api.user.setUserMetadata('verified', true)
+    } else {
+        api.access.deny("Vous devez vérifier votre statut étudiant pour vous connecter")
+    }
 }
