494b1362da72e105b581670dec16fdfca2491047
finxol <personnal@bottleneck.anonaddy.com>
2022-07-27 23:13:59 +0200

    extract universities list to file

---
+++ api/universities.ts
@@ -0,0 +1,13 @@
+/**
+ * List and email format of all supported universities
+ */
+const universities = [
+    {
+        id: 'ubs',
+        name: 'Université de Bretagne Sud',
+        country: 'France',
+        format: /^[A-Za-z\-]+\.e\d{7}@etud\.univ-ubs\.fr$/
+    }
+]
+
+export default universities
