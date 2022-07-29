dfdcf454eaeef234893456bb851eafff5a6c351b
finxol <personnal@bottleneck.anonaddy.com>
2022-07-29 05:02:04 +0200

    add jsdoc

--- api/errors/BookingError.ts
+++ api/errors/BookingError.ts
@@ -1,5 +1,9 @@
 // define new error
 export class BookingError extends Error {
+    /**
+     * Create the error
+     * @param message The error message
+     */
     constructor(message: string) {
         super(message)
     }
