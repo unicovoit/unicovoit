12cbfe627a9bf853bbdfe016a883bcd93c05fe78
finxol <personnal@bottleneck.anonaddy.com>
2022-08-01 03:42:35 +0200

    change text

--- pages/verify.vue
+++ pages/verify.vue
@@ -6,20 +6,15 @@
             Vérification
         </h2>
 
-        <p
-            class="text-body-2 mt-3 mb-5"
-        >
-            Avant de pouvoir utiliser UniCovoit, vous devez prouver votre statut étudiant.
-        </p>
 
         <div
             v-if="verified"
         >
             <v-alert
-                icon="mdi-check-circle"
-                type="success"
                 border="left"
+                icon="mdi-check-circle"
                 text
+                type="success"
             >
                 Statut étudiant vérifié
             </v-alert>
@@ -30,19 +25,36 @@
         >
             <v-window
                 v-model="step"
+                v-touch="{
+                    right: () => {},
+                    left: () => {},
+                }"
             >
                 <v-window-item
                     :value="1"
                 >
+                    <p
+                        class="text-body-1 mt-3 mb-2"
+                    >
+                        Avant de pouvoir utiliser UniCovoit, vous devez vérifier votre statut étudiant.
+                    </p>
+                    <p
+                        class="text-caption text--secondary mb-10"
+                    >
+                        Nous utilisons la vérification de votre adresse mail étudiante afin de valider votre profil
+                        étudiant.
+                        Cette vérification est renouvelée chaque année.<br>
+                        Nous n'utiliserons pas cette adresse à l'avenir.
+                    </p>
                     <h6
                         class="text-h6 font-weight-regular justify-space-between"
                     >
                         Entrez votre adresse mail universitaire
                     </h6>
                     <v-text-field
+                        ref="email"
                         v-model="email"
                         :rules="rules"
-                        ref="email"
                         label="Adresse mail"
                         placeholder="nom.e2111111@etud.univ-ubs.fr"
                     ></v-text-field>
@@ -55,24 +67,22 @@
                         <v-btn
                             :loading="loading"
                             color="primary"
-                            fab
-                            small
                             elevation="0"
+                            outlined
+                            rounded
                             @click="sendCode"
                         >
-                            <v-icon>
-                                mdi-arrow-right
-                            </v-icon>
+                            Vérifier
                         </v-btn>
                     </div>
 
                     <v-alert
                         v-if="error"
+                        border="left"
                         class="mt-5"
                         icon="mdi-alert-circle"
-                        type="error"
-                        border="left"
                         text
+                        type="error"
                     >
                         {{ error }}
                     </v-alert>
@@ -86,19 +96,24 @@
                     >
                         Entrez le code reçu par mail
                     </h6>
+                    <p
+                        ref="timer"
+                    >
+                        {{ timer }}
+                    </p>
                     <v-otp-input
-                        class="my-4"
                         v-model="code"
                         :disabled="loading"
+                        class="my-4"
                         @finish="verify"
                     ></v-otp-input>
                     <v-overlay
-                        absolute
                         :value="loading"
+                        absolute
                     >
                         <v-progress-circular
-                            indeterminate
                             color="primary"
+                            indeterminate
                         ></v-progress-circular>
                     </v-overlay>
 
@@ -117,11 +132,11 @@
 
                     <v-alert
                         v-if="error"
+                        border="left"
                         class="mt-5"
                         icon="mdi-alert-circle"
-                        type="error"
-                        border="left"
                         text
+                        type="error"
                     >
                         Le code entré est incorrect.<br>
                         <span
@@ -145,6 +160,7 @@ export default {
         return {
             verified: false,
             token: this.$route.query.token,
+            timer: '',
             step: 1,
             loading: false,
             error: false,
@@ -156,7 +172,9 @@ export default {
                     Votre université n'est peut-être pas encore prise en charge par UniCovoit.`,
                 exists: `L'adresse mail entrée est déjà utilisée.`,
                 generic: `Une erreur est survenue. Merci de réessayer plus tard.`
-            }
+            },
+            countdownInterval: setInterval(() => {}, 0),
+            countdownDate: new Date(),
         }
     },
     computed: {
@@ -171,12 +189,34 @@ export default {
         this.fetch()
     },
     methods: {
+        countdown() {
+            let now = new Date()
+            this.countdownDate = new Date().setMinutes(now.getMinutes() + 5)
+            clearInterval(this.countdownInterval)
+            this.countdownInterval = setInterval(function () {
+                let now = new Date().getTime()
+                let timeleft = this.countdownDate - now
+
+                // Calculating the days, hours, minutes and seconds left
+                let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))
+                let seconds = Math.floor((timeleft % (1000 * 60)) / 1000)
+
+                // Result is output to the specific element
+                this.timer = `${minutes}:${seconds}`
+
+                // Display the message when countdown is over
+                if (timeleft < 0) {
+                    clearInterval(this.countdownInterval);
+                    this.timer = "Le code a expiré."
+                }
+            }, 1000);
+        },
         async fetch() {
             if (this.token) {
                 const axios = this.$axios.create()
                 delete axios.defaults.headers.common['Authorization']
                 delete axios.defaults.headers.common['Cookie']
-                let { data } = await axios.get('/api/v1/users/isVerified', {
+                let {data} = await axios.get('/api/v1/users/isVerified', {
                     headers: {
                         Authorization: 'Bearer ' + this.token,
                         Cookie: undefined
@@ -221,9 +261,10 @@ export default {
                 this.error = false
                 this.step = 2
                 this.token = res.data.token
+                this.countdown()
                 setTimeout(() => {
                     this.resend = true
-                }, 6000)
+                }, 15000)
             }).catch(err => {
                 this.loading = false
                 if (err.response.status === 418) {
