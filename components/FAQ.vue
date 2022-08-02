7b37158d125dfabf768e9ecac6544984237aa8ad
finxol <personnal@bottleneck.anonaddy.com>
2022-08-03 01:40:09 +0200

    add & modify questions

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -44,13 +44,26 @@ export default {
             faq: [
                 {
                     title: "Comment fonctionne UniCovoit ?",
-                    content: `Avec UniCovoit, vous pouvez proposer vos trajets, qu'ils soient courts ou longs, aux autres étudiants afin de couvrir vos frais.
-                        Vous pouvez également réserver des trajets si vous cherchez à effectuer un déplacement sans avoir à conduire.<br>
-                        UniCovoit ne prend pas en charge les paiements donc vous devez convenir d'un moyen de paiement entre conducteur et passagers.
+                    content: `Avec UniCovoit, vous pouvez proposer vos trajets, qu'ils soient courts ou longs, à d'autres étudiants afin de couvrir vos frais.
+                        Vous pouvez également trouver des conducteurs pour vos trajets.<br>
                         Aucune commission n'est relevée sur le prix du trajet.`
                 }, {
                     title: "Est-ce que je peux réserver plusieurs places sur un trajet ?",
-                    content: `Non. Les trajets sont réservés aux étudiants, donc vous devez avoir un compte UniCovoit et montrer votre statut étudiant avant de pouvoir réserver un trajet.`
+                    content: `Non. Les trajets sont réservés aux étudiants, donc vous devez avoir un compte UniCovoit et vérifier votre statut étudiant avant de pouvoir réserver un trajet.`
+                }, {
+                    title: "Comment est-ce que UniCovoit se finance ?",
+                    content: `UniCovoit ne prend aucune commission sur le prix du trajet et ne revend aucune donnée.
+                        Le financement est assuré seulement par des dons. Si vous souhaitez soutenir UniCovoit, vous pouvez le faire sur <a href="https://ko-fi.com/unicovoit" target="_blank" rel="noopener noreferrer">Ko-Fi</a>.`
+                }, {
+                    title: "Quelle   forme de paiement est utilisée sur UniCovoit ?",
+                    content: `UniCovoit ne prend pas en charge les paiements donc vous devez convenir d'un moyen de paiement avec votre conducteur ou passager.`
+                }, {
+                    title: "Les utilisateurs sont-ils tous des étudiants ?",
+                    content: `Oui. Il est obligatoire de vérifier son statut étudiant avant de faire une réservation ou déposer une annonce.
+                        Cette vérification se fait par votre adresse mail étudiante.`
+                }, {
+                    title: "Comment puis-je contacter mon conducteur/passager ?",
+                    content: `Les coordonnées de chacun seront transmises par UniCovoit au moment de la réservation.`
                 }, {
                     title: "Qui a développé cette application ?",
                     content: "Cette application a été développée par finxol, un étudiant de l'IUT de Vannes (Bretagne)."
@@ -58,15 +71,12 @@ export default {
                     title: "Comment traitez-vous mes données ?",
                     content: `Les informations concernant les trajets et les utilisateurs sont stockées sur les serveurs d'UniCovoit en Allemagne. Ces informations ne sont pas communiquées à des tiers.<br>
                         Les informations de connexion (identifiant, mot de passe) sont gérées par Auth0 sur des serveurs en Europe.<br>
-                        Vous trouverez plus de détails dans notre <a href='/legal/privacy' class="text--secondary">Politique de Confidentialité</a>.`
-                }, {
-                    title: "Comment supprimer mon compte ?",
-                    content: `Accédez à votre profil, puis dans l'onglet "Paramètres", cliquez sur "Supprimer mon compte.`
+                        Vous trouverez plus de détails dans notre <a href='/legal/privacy'>Politique de Confidentialité</a>.`
                 }, {
                     icon: "mdi-message-processing-outline",
                     title: "Comment puis-je contacter l'équipe ?",
-                    content: `Vous pouvez envoyer un mail à <a class="text--secondary" href="mailto:support@unicovoit.fr" target="_blank" rel="noopener noreferrer">support@unicovoit.fr</a>.
-                        Vous pouvez également m'envoyer un message sur Discord avec finxol#8918 ou <a class="text--secondary" href="https://twitter.com/_finxol" rel="noopener noreferrer" target="_blank">Twitter</a>.`
+                    content: `Vous pouvez envoyer un mail à <a href="mailto:support@unicovoit.fr" target="_blank" rel="noopener noreferrer">support@unicovoit.fr</a>,
+                        ou nous contacter sur <a href="https://instagram.com/unicovoit" target="_blank" rel="noopener noreferrer">Instagram</a>.`
                 }
             ]
         }
