ee26c9212d2e03b3b1b60b7b41976fe379406dcd
finxol <personnal@bottleneck.anonaddy.com>
2022-08-04 19:53:06 +0200

    add advantages

--- pages/index.vue
+++ pages/index.vue
@@ -40,11 +40,47 @@
         </v-container>
 
 
+        <v-container>
+            <v-row
+                v-for="advantage in advantages"
+                :key="advantage.title"
+                class="d-flex align-start mb-2"
+            >
+                <v-col
+                    cols="2"
+                >
+                    <v-icon size="35">{{ advantage.icon }}</v-icon>
+                </v-col>
+                <v-col
+                    class="px-0"
+                    cols="9"
+                >
+                    <h6 class="text-h6">
+                        {{ advantage.title }}
+                    </h6>
+                    <p
+                        class="mb-0"
+                    >
+                        {{ advantage.content }}
+                    </p>
+                </v-col>
+            </v-row>
+
+            <NuxtLink
+                class="text-decoration-none text--secondary"
+                to="/about"
+            >
+                <v-icon>mdi-information</v-icon>
+                En savoir plus
+            </NuxtLink>
+        </v-container>
+
+
         <v-card
+            id="presentation"
             :color="`primary ${$vuetify.theme.dark ? 'darken-4' : 'lighten-5'}`"
             flat
             rounded="20"
-            id="presentation"
         >
             <v-card-title
                 class="text-h4 mb-5"
@@ -58,15 +94,18 @@
                     UniCovoit est une plateforme de covoiturage pour les étudiants.
                 </p>
                 <p>
-                    L'objectif est d'encourager le covoiturage entre étudiants, sans prendre de commission sur le prix du trajet.
+                    L'objectif est d'encourager le covoiturage entre étudiants, sans prendre de commission sur le prix
+                    du trajet.
                 </p>
                 <p>
-                    Plus d'informations disponibles sur <NuxtLink alt="About" class="text--secondary" to="/about">cette page</NuxtLink>.
+                    Plus d'informations disponibles sur
+                    <NuxtLink alt="About" class="text--secondary" to="/about">cette page</NuxtLink>
+                    .
                 </p>
             </v-card-text>
         </v-card>
 
-        <FAQ class="mt-5" />
+        <FAQ class="mt-5"/>
 
         <v-container
             class="text--secondary text-caption ma-0 pa-0 mt-10"
@@ -74,20 +113,22 @@
             <div>
                 <v-icon
                     size="18"
-                >mdi-image-outline</v-icon>
+                >mdi-image-outline
+                </v-icon>
                 Illustration de la collection
-                <a class="text--secondary" target="_blank" rel="noreferrer noopener"
-                   href="https://icons8.com/illustrations">
+                <a class="text--secondary" href="https://icons8.com/illustrations" rel="noreferrer noopener"
+                   target="_blank">
                     Ouch!
                 </a>
             </div>
             <div>
                 <v-icon
                     size="18"
-                >mdi-image-outline</v-icon>
+                >mdi-image-outline
+                </v-icon>
                 Logo par
-                <a class="text--secondary" target="_blank" rel="noreferrer noopener"
-                   href="https://www.instagram.com/feun_art/">
+                <a class="text--secondary" href="https://www.instagram.com/feun_art/" rel="noreferrer noopener"
+                   target="_blank">
                     Enora Couloigner
                 </a>
             </div>
@@ -101,6 +142,19 @@ export default {
     auth: false,
     data: () => ({
         snackbar: false,
+        advantages: [{
+            icon: 'mdi-lock',
+            title: 'Sécurité',
+            content: `Profitez de trajets exclusivement entre étudiants.`
+        }, {
+            icon: 'mdi-piggy-bank',
+            title: 'Petits Prix',
+            content: `UniCovoit contribue à maintenir des prix bas en ne prenant aucune commission sur les trajets.`
+        }, {
+            icon: 'mdi-account-multiple-check',
+            title: 'Transparence',
+            content: `UniCovoit est conçu pour des étudiants, par des étudiants. Le code de l'application est également entièrement ouvert.`
+        }]
     }),
     computed: {
         isLoggedIn() {
@@ -123,7 +177,7 @@ export default {
 }
 </script>
 
-<style scoped lang="sass">
+<style lang="sass" scoped>
 /*h2
     background-image: url('/landscape.svg')
     background-size: cover
