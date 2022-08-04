fb83dd5a5601b33aaa244cb9cc9690412ca11897
finxol <personnal@bottleneck.anonaddy.com>
2022-08-04 19:52:40 +0200

    add email component

---
+++ components/EmailAddress.vue
@@ -0,0 +1,18 @@
+<template>
+    <a
+        href="mailto:support@unicovoit.fr"
+        class="text--secondary text-decoration-underline"
+    >
+        support@unicovoit.fr
+    </a>
+</template>
+
+<script>
+export default {
+    name: "EmailAddress"
+}
+</script>
+
+<style scoped>
+
+</style>
