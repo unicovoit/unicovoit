355931279dbb7cc0236db00c41c7806f23a52f0e
finxol <personnal@bottleneck.anonaddy.com>
2022-08-07 23:23:23 +0200

    reorganise questions

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <div class="text-h4">
+        <div class="text-h4 ml-1">
             FAQ
         </div>
         <v-row
@@ -48,6 +48,16 @@ export default {
                         Vous pouvez également trouver des conducteurs pour vos trajets.<br>
                         Aucune commission n'est relevée sur le prix du trajet.`
                 }, {
+                    title: "Les utilisateurs sont-ils tous des étudiants ?",
+                    content: `Oui. Il est obligatoire de vérifier son statut étudiant avant de faire une réservation ou déposer une annonce.
+                        Cette vérification se fait par votre adresse mail étudiante.`
+                }, {
+                    title: "Quelle forme de paiement est utilisée sur UniCovoit ?",
+                    content: `UniCovoit ne prend pas en charge les paiements donc vous devez convenir d'un moyen de paiement avec votre conducteur ou passager.`
+                }, {
+                    title: "Comment puis-je contacter mon conducteur/passager ?",
+                    content: `Les coordonnées de chacun seront transmises par UniCovoit au moment de la réservation.`
+                }, {
                     title: "Est-ce que je peux réserver plusieurs places sur un trajet ?",
                     content: `Non. Les trajets sont réservés aux étudiants, donc vous devez avoir un compte UniCovoit et vérifier votre statut étudiant avant de pouvoir réserver un trajet.`
                 }, {
@@ -55,26 +65,16 @@ export default {
                     content: `UniCovoit ne prend aucune commission sur le prix du trajet et ne revend aucune donnée.
                         Le financement est assuré seulement par des dons. Si vous souhaitez soutenir UniCovoit, vous pouvez le faire sur <a href="https://ko-fi.com/unicovoit" target="_blank" rel="noopener noreferrer">Ko-Fi</a>.`
                 }, {
-                    title: "Quelle   forme de paiement est utilisée sur UniCovoit ?",
-                    content: `UniCovoit ne prend pas en charge les paiements donc vous devez convenir d'un moyen de paiement avec votre conducteur ou passager.`
-                }, {
-                    title: "Les utilisateurs sont-ils tous des étudiants ?",
-                    content: `Oui. Il est obligatoire de vérifier son statut étudiant avant de faire une réservation ou déposer une annonce.
-                        Cette vérification se fait par votre adresse mail étudiante.`
-                }, {
-                    title: "Comment puis-je contacter mon conducteur/passager ?",
-                    content: `Les coordonnées de chacun seront transmises par UniCovoit au moment de la réservation.`
-                }, {
-                    title: "Qui a développé cette application ?",
-                    content: "Cette application a été développée par finxol, un étudiant de l'IUT de Vannes (Bretagne)."
-                }, {
                     title: "Comment traitez-vous mes données ?",
                     content: `Les informations concernant les trajets et les utilisateurs sont stockées sur les serveurs d'UniCovoit en Allemagne. Ces informations ne sont pas communiquées à des tiers.<br>
                         Les informations de connexion (identifiant, mot de passe) sont gérées par Auth0 sur des serveurs en Europe.<br>
                         Vous trouverez plus de détails dans notre <a href='/legal/privacy'>Politique de Confidentialité</a>.`
                 }, {
+                    title: "Qui a développé cette application ?",
+                    content: "Cette application a été développée par finxol, un étudiant de l'IUT de Vannes (Bretagne)."
+                }, {
                     icon: "mdi-message-processing-outline",
-                    title: "Comment puis-je contacter l'équipe ?",
+                    title: "Comment puis-je contacter UniCovoit ?",
                     content: `Vous pouvez envoyer un mail à <a href="mailto:support@unicovoit.fr" target="_blank" rel="noopener noreferrer">support@unicovoit.fr</a>,
                         ou nous contacter sur <a href="https://instagram.com/unicovoit" target="_blank" rel="noopener noreferrer">Instagram</a>.`
                 }
